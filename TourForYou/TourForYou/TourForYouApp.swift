9873274bce9bc363ef5e70c12327054a994d5054
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-08-18 16:11:25 +0900

    feat: 프로젝트 생성

---
+++ TourForYou/TourForYou/TourForYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourForYouApp.swift
+//  TourForYou
+//
+//  Created by Admin on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TourForYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
